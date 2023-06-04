60f1d834751094d6b7def7f18b57c6c1c594be61
Leo Zhou <leozhou4@gmail.com>
2023-06-04 18:39:08 +1000

    added modal to show all all assosciated data, changed text formatting

--- public/script.js
+++ public/script.js
@@ -1,19 +1,20 @@
-///// Display time and date
-/////
+////////////////////////
+// Display time and date
+////////////////////////
 const date = new Date();
 
 // Get time and date values
-let hour = date.getHours();
-let minute = date.getMinutes();
+let time = date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }) // Display time in 12 hour
 let day = date.getDate();
 let month = date.getMonth() + 1;
-let year = date.getFullYear();
+let year = date.getFullYear().toString().substring(2); // Only display last 2 digits of the year
 
 // Date Format
-let currentDate = `${day}/${month}/${year} - ${hour}:${minute}`;
+let currentDate = `${time} - ${day}/${month}/${year}`;
 
-///// Array to store user input data
-/////
+/////////////////////////////////
+// Array to store user input data
+/////////////////////////////////
 // var taskList = [];
 
 // Function to add task to the list
@@ -53,9 +54,9 @@ function addTask(name, type, reps, sets, weight, weightMeasurement, goal, diffic
     // taskList.push(task);
     displayTasks();
 }
-
-///// Add input values into array
-/////
+//////////////////////////////
+// Add input values into array
+//////////////////////////////
 // Variable for DOM selection of HTML elements
 const form = document.querySelector('#taskform');
 
@@ -77,13 +78,14 @@ form.addEventListener('submit', function(event) {
     )
     
     // Close the modal when submit button is pressed
-    modal.close();
+    inputModal.close();
 
     }
 )
 
-///// Display data from array in task list
-/////
+///////////////////////////////////////
+// Display data from array in task list
+///////////////////////////////////////
 // Variable for DOM selection of HTML elements
 const tasklistElem = document.querySelector('#tasklist');
 
@@ -95,28 +97,103 @@ function displayTasks() {
     if (localTasks !== null) {
         localTasks.forEach((task) => {
 
+            // Log each task in the console
             console.log(task);
 
-            // Create task items for the DOM and add to the list
+            // Create summarised task items for the DOM and add to the list
             let item = document.createElement('li');
             item.setAttribute('data-id', task.id);
-            item.innerHTML = `<p>
-            <strong>${task.date}</strong>
-            <br>${task.name}
-            <br>${task.type}
-            <br>${task.reps}
-            <br>${task.sets}
-            <br>${task.weight}${task.weightMeasurement}
-            <br>${task.goal}
-            <br>${task.difficulty}
-            <br>${task.note}
-            </p>`;
+            item.innerHTML = `
+                <h2>
+                    <strong>${task.name}</strong>
+                </h2>
+
+                <p>
+                    ${task.type}
+                    <br>
+                    <strong>${task.date}</strong> 
+                    <br>
+                    <br>
+                    <strong>${task.weight}</strong>${task.weightMeasurement} 
+                    <br>
+                    <strong>${task.reps} </strong> Reps x <strong>${task.sets}</strong> Sets
+                </p>
+            `;
             tasklistElem.appendChild(item);
 
-            // Clear form inputs after task gets displayed
-            form.reset(); 
+            /////////////////////////////////////////////////////////////////////////////
+            // Create dialog element for each list item containing full list of task data
+            /////////////////////////////////////////////////////////////////////////////
+            let moreModal = document.createElement('dialog');
+            moreModal.innerHTML = `
+            <h1>
+            <strong>${task.name}</strong>
+            </h1>
 
+            <h2>
+                ${task.type} 
+                <br>
+                <strong>${task.date}</strong>
+            </h2>
+
+            <h3>
+                <strong>${task.weight}</strong>${task.weightMeasurement} 
+                <br>
+                <strong>${task.reps} </strong> Reps x <strong>${task.sets}</strong> Sets
+            </h3>
+
+            <p>
+                <strong>Goal:</strong> ${task.goal} 
+                <br>
+                <strong>Difficulty:</strong> ${task.difficulty} 
+                <br>
+                <strong>Note:</strong> ${task.note}
+            </p>
+            `;
+            item.appendChild(moreModal);
+
+            // Create a close button inside the view more modal
+            let closeMoreModal = document.createElement('button');
+            let closeMoreModalText = document.createTextNode('Close');
+            closeMoreModal.appendChild(closeMoreModalText);
+            moreModal.appendChild(closeMoreModal);
+
+            // Event listener to close the view more modal when clicked
+            closeMoreModal.addEventListener('click', () => {
+                moreModal.close();
+                }
+            );
+
+            // Close modal when clicking outside
+            // Reference: https://blog.webdevsimplified.com/2023-04/html-dialog/
+            moreModal.addEventListener("click", e => {
+                const dialogDimensions = moreModal.getBoundingClientRect()
+                if (
+                e.clientX < dialogDimensions.left ||
+                e.clientX > dialogDimensions.right ||
+                e.clientY < dialogDimensions.top ||
+                e.clientY > dialogDimensions.bottom
+                ) {
+                    moreModal.close()
+                    }
+                }
+            );
+
+            // Create 'view more' button for each list item that is created, to open the 'view more' modal
+            let moreButton = document.createElement('button');
+            let moreButtonText = document.createTextNode('View More');
+            moreButton.appendChild(moreButtonText);
+            item.appendChild(moreButton);
+
+            // Event listener to show the 'view more' modal when the 'view more' button is clicked
+            moreButton.addEventListener('click', () => {
+                moreModal.showModal();
+                }
+            );
+
+            //////////////////////////////////////////////////////////
             // Create delete button for each list item that is created
+            //////////////////////////////////////////////////////////
             let delButton = document.createElement('button');
             let delButtonText = document.createTextNode('Remove');
             delButton.appendChild(delButtonText);
@@ -137,35 +214,57 @@ function displayTasks() {
                 item.remove();
                 }
             )
+            
+            // Clear form inputs after task gets displayed
+            form.reset(); 
             }
         )
     }
 }
 
-// Test values
-// addTask("Exercise", "Too Easy", 777, 777, 777); 
-
 // Call displayTask function see previous entries when page loads
-displayTasks()
+displayTasks();
 
-///// Modal
-///// 
+///////////////////
+// Input Form Modal
+///////////////////
 // Variable for DOM selection of HTML elements
-const modal = document.querySelector("#data-modal");
-const openModal = document.querySelector("#data-open-modal");
-const closeModal = document.querySelector("#data-close-modal");
+const inputModal = document.querySelector("#input-modal");
 
 // Event listener to show modal when button is clicked
-openModal.addEventListener("click", () => {
-    modal.showModal();
+const openInputModal = document.querySelector("#open-input-modal");
+
+openInputModal.addEventListener("click", () => {
+    inputModal.showModal();
 }
 )
 
 // Event listener to close modal when close button is clicked
-closeModal.addEventListener("click", () => {
-    modal.close();
+const closeInputModal = document.querySelector("#close-input-modal");
+
+closeInputModal.addEventListener("click", () => {
+    inputModal.close();
 
     // Clear form inputs when submit button is pressed
     form.reset(); 
 }
 )
+
+// Close modal when clicking outside
+// Reference: https://blog.webdevsimplified.com/2023-04/html-dialog/
+inputModal.addEventListener("click", e => {
+    const dialogDimensions = inputModal.getBoundingClientRect()
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+        inputModal.close()
+
+    // Clear form inputs when modal is closed
+    form.reset(); 
+    }
+  })
+
+
